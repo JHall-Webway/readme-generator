42a7739d8558bfe6351aacc9831d9ab486cc39be
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-08-03 18:28:25 -0700

    add questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,159 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?(required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'repo',
+            message: 'What is the link to your repository?(if public available)'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project(required)',
+            validate: description => {
+                if (description) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'table',
+            message: 'Would you like a table of contents?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'installConfirm',
+            message: 'Does your project need to be installed?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Provide detailed installation instructions',
+            when: ({ installConfirm }) => {
+                if (installConfirm) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Describe how to use this project(required)',
+            validate: useDescription => {
+                if (useDescription) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'screenshotConfirm',
+            message: 'Would you like to include a screenshot in your usage description?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'screenshotDirectory',
+            message: 'Please enter in the location of your screenshot',
+            when: ({ screenshotConfirm }) => {
+                if (screenshotConfirm) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license you would like to use',
+            choices: ['ISC', 'MIT', 'GNU GPLv3', 'Other']
+        },
+        {
+            type: 'input',
+            name: 'licenseInput',
+            message: 'Please enter a Title for your custom license',
+            when: ({ license }) => {
+                if (license === 'Other') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'licenseCopyRightDate',
+            message: 'What year are we marking the Copyright year for?',
+            default: new Date().getFullYear(),
+            when: ({ licenseInput }) => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'licenseBodyInput',
+            message: 'Please enter the body of your custom license',
+            when: ({ licenseInput }) => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubLink',
+            message: 'Please enter the link to your GitHub profile(if you have one)',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please input an Email that you would like anyone to contact you at(if you have one)'
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return questions();
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(answers => console.log(answers));
---
+++ Develop/test.js
@@ -0,0 +1,21 @@
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const testObj = {
+    name: "README Generator",
+    repo: "https://github.com/JHallUofA/readme-generator",
+    description: "This readme generator has the ability to dynamically create readme files with minimal user input",
+    table: true,
+    installConfirm: true,
+    install: "Follow the instructions on the npm website in order to install",
+    usage: "Use this application to dynamically generate professional README files using minimal user input, README will generate when all inputs are finished",
+    screenshotConfirm: true,
+    screenshotDirectory: './images/screenshot.png',
+    license: 'Other',
+    licenseInput: 'Custom Title',
+    licenseCopyRightDate: 2021,
+    licenseBodyInput: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    githubLink: 'https://github.com/JHallUofA',
+    email: 'mr.sl3go.jh@gmail.com'
+};
+
+console.log(generateMarkdown(testObj));
